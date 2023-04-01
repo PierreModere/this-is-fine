e7534da894f59e29b6eee3c68b40e8f868de24b5
Pierre Modéré <pierre.martiniere72@gmail.com>
2023-04-02 01:24:56 +0200

    Fix scrolling background :))

--- Assets/Scripts/StartMenu/ScrollingBg.cs
+++ Assets/Scripts/StartMenu/ScrollingBg.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
- 
-public class Scroller : MonoBehaviour {
-    [SerializeField] private SpriteRenderer _spriterRenderer;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] private RawImage _img;
     [SerializeField] private float _x, _y;
- 
+
     void Update()
     {
-        _spriterRenderer.size = new Vector2(_spriterRenderer.size.x + _x * Time.deltaTime,_spriterRenderer.size.y + _y * Time.deltaTime);
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
     }
 }
- 
