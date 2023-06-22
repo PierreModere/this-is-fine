9e85802d7866b045929355a92aa4eaae8212fbab
Pierre Modéré <pierre.martiniere72@gmail.com>
2023-06-23 00:29:46 +0200

    Add planet translation animation & add player readiness check before launching minigame + MARIO POINT DE CROIX O.O

--- Assets/Scripts/ToggleMenu.cs
+++ Assets/Scripts/ToggleMenu.cs
@@ -8,6 +8,14 @@ public class ToggleMenu : MonoBehaviour
     [SerializeField]
     private GameObject menuToEnable;
 
+    private GameObject planetBg;
+    float planetDefaultY = -245;
+    float planetTargetY = 465;
+    private void Start()
+    {
+        if (GameObject.Find("background")) planetBg = GameObject.Find("background");
+    }
+
     public void disableMenu()
     {
         if (menuToDisable.name != "StartMenuCanvas")
@@ -15,11 +23,21 @@ public class ToggleMenu : MonoBehaviour
             menuToDisable.SetActive(false);
             menuToDisable.tag = "Untagged";
         }
+
+        if (menuToEnable.name == "MenuCanvas" && planetBg != null)
+        {
+            planetBg.transform.DOLocalMoveY(planetDefaultY, 0.8f);
+        }
     }
     public void enableMenu()
     {
         menuToEnable.SetActive(true);
         menuToEnable.transform.DOScale(new Vector3(1.05f, 1.05f, 1.05f), 0.1f).OnComplete(() => { menuToEnable.transform.DOScale(new Vector3(1f, 1f, 1f), 0.15f); });
         menuToEnable.tag = "activeScreen";
+
+        if ((menuToEnable.name == "CreateRoomCanvas" || menuToEnable.name == "JoinRoomCanvas") && planetBg!= null)
+        {
+            planetBg.transform.DOLocalMoveY(planetTargetY, 0.8f);
+        }
     }
 }
